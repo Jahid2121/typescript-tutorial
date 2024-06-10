cfa5449a829e245eb40dc1fee58f288896c6bea8
Jahid2121 <138579420+Jahid2121@users.noreply.github.com>
2024-06-10 08:51:22 +0600

    DOM manupulate with Typescript

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,16 @@
 "use strict";
 const userForm = document.querySelector(".user-form");
 const userName = document.querySelector("#name");
-console.log(userName);
-userForm.addEventListener("submit", (e) => {
-    e.preventDefault();
+const userCountry = document.querySelector("#country");
+const userEmail = document.querySelector("#email");
+const userFeedback = document.querySelector("#feedback");
+userForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const data = {
+        username: userName.value,
+        useremail: userEmail.value,
+        usercountry: userCountry.value,
+        userfeedback: userFeedback.value
+    };
+    console.log(data);
 });
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,21 @@
 const userForm = document.querySelector(".user-form") as HTMLFormElement;
 
 const userName = document.querySelector("#name") as HTMLInputElement;
-console.log(userName);
 
-userForm.addEventListener("submit", (e) => {
-    e.preventDefault();
+const userCountry = document.querySelector("#country") as HTMLSelectElement;
+
+const userEmail = document.querySelector("#email") as HTMLInputElement;
+
+const userFeedback = document.querySelector("#feedback") as HTMLTextAreaElement;
+
+
+userForm.addEventListener("submit", (event: Event) => {
+    event.preventDefault();
+    const data = {
+        username: userName.value,
+        useremail: userEmail.value,
+        usercountry: userCountry.value,
+        userfeedback: userFeedback.value
+    }
+    console.log(data);
 })
